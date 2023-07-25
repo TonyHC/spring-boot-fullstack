4c8e8933200f9a59e70b667d3ef253cd6a2fcf38
Tony Chao <tchao@mail.usf.edu>
2023-07-25 19:05:30 -0400

    Add and configure Spring Boot Actuator to expose the health endpoint
    Expose the health endpoint for the backend API deployed to AWS Elastic Beanstalk to communicate with the Elastic Load Balancer.

--- backend/src/main/java/com/tonyhc/springbootdemo/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/tonyhc/springbootdemo/security/SecurityFilterChainConfig.java
@@ -34,6 +34,7 @@ public class SecurityFilterChainConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/v1/customers", "/api/v1/auth/login").permitAll()
                         .anyRequest().authenticated()
                 )
