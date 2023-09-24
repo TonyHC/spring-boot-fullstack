81acede3e16698c85ffdd7cff0ba7bb5b97650d4
Tony Chao <tchao@mail.usf.edu>
2023-09-24 19:29:34 -0400

    Update the correct import for the CustomerSlice

--- frontend/react/src/store/customer/CustomerReducer.test.ts
+++ frontend/react/src/store/customer/CustomerReducer.test.ts
@@ -1,5 +1,6 @@
+
 import {expect, test} from "vitest";
-import reducer, {resetErrorState, updateCustomerPageState} from "./CustomerSlice.ts";
+import reducer, {resetErrorState, updateCustomerPageState} from "./CustomerSlice.tsx";
 import {Customer, ServerError} from "../../types.ts";
 
 describe('CustomerReducer tests', () => {
@@ -22,44 +23,44 @@ describe('CustomerReducer tests', () => {
     });
 
     test('it should reset the error state', () => {
-       const previousState = {
-           customerPage: {
-               customers: [],
-               currentPage: 0,
-               totalItems: 0,
-               totalPages: 0,
-               pageSize: 10,
-               sort: 'customer_id,ASC',
-               query: ''
-           },
-           customers: [],
-           customer: {} as Customer,
-           error: {
-               timestamp: '2023-09-01T09:09:46.419221',
-               statusCode: 404,
-               message: 'Customer with id [12a] was not found',
-               path: '/api/v1/customers/12a'
-           },
-           status: 'idle',
-           actionType: ''
-       };
+        const previousState = {
+            customerPage: {
+                customers: [],
+                currentPage: 0,
+                totalItems: 0,
+                totalPages: 0,
+                pageSize: 10,
+                sort: 'customer_id,ASC',
+                query: ''
+            },
+            customers: [],
+            customer: {} as Customer,
+            error: {
+                timestamp: '2023-09-01T09:09:46.419221',
+                statusCode: 404,
+                message: 'Customer with id [12a] was not found',
+                path: '/api/v1/customers/12a'
+            },
+            status: 'idle',
+            actionType: ''
+        };
 
-       expect(reducer(previousState, resetErrorState)).toEqual({
-           customerPage: {
-               customers: [],
-               currentPage: 0,
-               totalItems: 0,
-               totalPages: 0,
-               pageSize: 10,
-               sort: 'customer_id,ASC',
-               query: ''
-           },
-           customers: [],
-           customer: {} as Customer,
-           error: {} as ServerError,
-           status: 'idle',
-           actionType: ''
-       });
+        expect(reducer(previousState, resetErrorState)).toEqual({
+            customerPage: {
+                customers: [],
+                currentPage: 0,
+                totalItems: 0,
+                totalPages: 0,
+                pageSize: 10,
+                sort: 'customer_id,ASC',
+                query: ''
+            },
+            customers: [],
+            customer: {} as Customer,
+            error: {} as ServerError,
+            status: 'idle',
+            actionType: ''
+        });
     });
 
     test('it should update customer page state', () => {
